057220d1f09032e9cf76595295b965f8543aa44f
SarthakSkamble <rohikamble17@gmail.com>
2026-03-06 04:46:33 +0530

    lead update

--- backend/controller/lead_update.js
+++ backend/controller/lead_update.js
@@ -1,6 +1,6 @@
 import prisma from "../prismaclient.js"
 export default async function update_lead(req,res){
-    const{lead_id,Name,salutation,title,description,lead_status,phone,email,address,source}=req.body
+    const{lead_id,Name,salutation,title,description,lead_status,phone,email,address,source,age,city,calls_made,emails_sent}=req.body
     const resp= await prisma.lead.update({
         where:{
              lead_id
@@ -14,7 +14,11 @@ export default async function update_lead(req,res){
             phone,
             email,
             address,
-            source
+            source,
+            age,
+            city,
+            calls_made,
+            emails_sent
 
         }
     })
---
+++ backend/prisma/migrations/20260305230229_lead/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Lead" ADD COLUMN     "age" INTEGER,
+ADD COLUMN     "calls_made" INTEGER,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "emails_sent" INTEGER;
